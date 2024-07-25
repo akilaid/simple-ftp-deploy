5a1c507a1e8bcb1ff4d96a77f2bab4931a80091b
AKILA INDUNIL <45641122+akilaid@users.noreply.github.com>
2024-07-25 18:25:50 +0530

    Update ftp_upload.py

--- ftp_upload.py
+++ ftp_upload.py
@@ -1,11 +1,19 @@
 import ftplib
 import os
+import hashlib
 import json
-import time
 
-def get_file_mod_times(directory, exclude_files=None, exclude_dirs=None):
-    """Get modification times for all files in the given directory, excluding specified files and directories."""
-    file_mod_times = {}
+def calculate_md5(file_path):
+    """Calculate MD5 hash of a file."""
+    md5_hash = hashlib.md5()
+    with open(file_path, 'rb') as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            md5_hash.update(byte_block)
+    return md5_hash.hexdigest()
+
+def generate_file_hashes(directory, exclude_files=None, exclude_dirs=None):
+    """Generate MD5 hashes for all files in the given directory, excluding specified files and directories."""
+    file_hashes = {}
     exclude_files = exclude_files or []
     exclude_dirs = exclude_dirs or []
     for root, dirs, files in os.walk(directory):
@@ -17,8 +25,8 @@ def get_file_mod_times(directory, exclude_files=None, exclude_dirs=None):
                 continue
             file_path = os.path.join(root, file_name)
             relative_path = os.path.relpath(file_path, directory)
-            file_mod_times[relative_path] = os.path.getmtime(file_path)
-    return file_mod_times
+            file_hashes[relative_path] = calculate_md5(file_path)
+    return file_hashes
 
 def create_ftp_directory(ftp, directory_path):
     """Create directory structure on FTP server."""
@@ -43,14 +51,16 @@ def upload_file(ftp, file_path, remote_path):
         ftp.storbinary(f'STOR {remote_path}', f)
     print(f'Uploaded file: {file_path}')
 
-def list_files_ftp(ftp, path):
-    """List all files in a given directory on the FTP server."""
-    file_list = []
-    try:
-        ftp.retrlines(f'LIST {path}', file_list.append)
-    except ftplib.error_perm as e:
-        print(f'Error listing files: {e}')
-    return file_list
+def delete_extra_files_on_ftp(ftp, server_file_hashes, local_file_hashes):
+    """Delete files from the FTP server that do not exist in the local files, only if they are explicitly listed."""
+    for file in server_file_hashes:
+        if file not in local_file_hashes:
+            print(f'Deleting extra file on server: {file}')
+            try:
+                ftp.delete(file)
+                print(f'Deleted file: {file}')
+            except ftplib.error_perm as e:
+                print(f'Error deleting file {file}: {e}')
 
 def list_files_in_directory(directory):
     """List all files in the given directory and its subdirectories."""
@@ -69,7 +79,7 @@ def main():
     ftp_password = os.getenv('FTP_PASSWORD')
     ftp_directory = os.getenv('FTP_DIR', './')
     local_directory = os.getenv('LOCAL_DIR', './')
-    mod_times_file_name = os.getenv('MOD_TIMES_FILE', '.file_mod_times.json')
+    hash_file_name = os.getenv('HASH_FILE', '.file_hashes.json')
 
     # Print environment variable values
     print(f'FTP Server: {ftp_server}')
@@ -77,7 +87,7 @@ def main():
     print(f'FTP Password: {ftp_password}')
     print(f'FTP Directory: {ftp_directory}')
     print(f'Local Directory: {local_directory}')
-    print(f'Modification Times File Name: {mod_times_file_name}')
+    print(f'Hash File Name: {hash_file_name}')
 
     # List files in the local directory
     print(f'Listing files in local directory: {local_directory}')
@@ -90,45 +100,39 @@ def main():
     ftp.set_pasv(True)
     print('Connected to FTP server.')
 
-    # List all files and folders on the FTP server before changing the directory
-    print('Listing all files and folders on the FTP server after authentication:')
-    all_files_on_ftp = list_files_ftp(ftp, '/')
-    for file in all_files_on_ftp:
-        print(file)
-
     # Change to the desired directory
     print(f'Changing to directory: {ftp_directory}')
     ftp.cwd(ftp_directory)
     print(f'Changed to directory: {ftp_directory}')
 
-    # Check for existing modification times file on the server
+    # Check for existing hash file on the server
     first_time_upload = False
     try:
-        print(f'Checking for existing modification times file: {mod_times_file_name}')
-        ftp.retrbinary(f'RETR {mod_times_file_name}', open(mod_times_file_name, 'wb').write)
-        with open(mod_times_file_name, 'r') as f:
-            server_file_mod_times = json.load(f)
-        print(f'Found existing modification times file: {mod_times_file_name}')
+        print(f'Checking for existing hash file: {hash_file_name}')
+        ftp.retrbinary(f'RETR {hash_file_name}', open(hash_file_name, 'wb').write)
+        with open(hash_file_name, 'r') as f:
+            server_file_hashes = json.load(f)
+        print(f'Found existing hash file: {hash_file_name}')
     except ftplib.error_perm:
-        print(f'No existing modification times file found. Assuming first-time upload.')
-        server_file_mod_times = {}
+        print(f'No existing hash file found. Assuming first-time upload.')
+        server_file_hashes = {}
         first_time_upload = True
 
-    # Get current file modification times, excluding the script file and specified directories
+    # Generate current file hashes, excluding the script file and specified directories
     script_file_name = 'ftp_upload.py'  # Assuming the script is named ftp_upload.py
-    print(f'Getting file modification times for local directory: {local_directory}')
-    current_file_mod_times = get_file_mod_times(local_directory, exclude_files=[script_file_name], exclude_dirs=['.git', '.github'])
-    print(f'Got file modification times: {current_file_mod_times}')
+    print(f'Generating file hashes for local directory: {local_directory}')
+    current_file_hashes = generate_file_hashes(local_directory, exclude_files=[script_file_name], exclude_dirs=['.git', '.github'])
+    print(f'Generated file hashes: {current_file_hashes}')
 
     # Determine files to upload
     files_to_upload = []
     if first_time_upload:
         print('First time upload: Uploading all files.')
-        files_to_upload = list(current_file_mod_times.keys())
+        files_to_upload = list(current_file_hashes.keys())
     else:
         print('Determining modified or new files...')
-        for file, mod_time in current_file_mod_times.items():
-            if current_file_mod_times[file] != server_file_mod_times.get(file):
+        for file, hash in current_file_hashes.items():
+            if current_file_hashes[file] != server_file_hashes.get(file):
                 print(f'File modified or new: {file}')
                 files_to_upload.append(file)
             else:
@@ -141,12 +145,17 @@ def main():
         remote_file_path = os.path.join(ftp_directory, file)
         upload_file(ftp, local_file_path, remote_file_path)
 
-    # Upload the modification times file
-    with open(mod_times_file_name, 'w') as f:
-        json.dump(current_file_mod_times, f)
-    print(f'Uploading modification times file: {mod_times_file_name}')
-    upload_file(ftp, mod_times_file_name, os.path.join(ftp_directory, mod_times_file_name))
-    print(f'Uploaded modification times file: {mod_times_file_name}')
+    # Delete extra files on the server
+    print('Deleting extra files on the server...')
+    delete_extra_files_on_ftp(ftp, server_file_hashes, current_file_hashes)
+    print('Deleted extra files on the server.')
+
+    # Upload the hash file
+    with open(hash_file_name, 'w') as f:
+        json.dump(current_file_hashes, f)
+    print(f'Uploading hash file: {hash_file_name}')
+    upload_file(ftp, hash_file_name, os.path.join(ftp_directory, hash_file_name))
+    print(f'Uploaded hash file: {hash_file_name}')
 
     # Close the connection
     print('Closing FTP connection...')
